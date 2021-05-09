4f145807bb03efcdd1b01553083d5c8938c2d1fa
Mikhail Klimenko <mklimenko@systematica-consulting.ru>
2021-05-09 13:19:02 +0300

    init

---
+++ src/main/java/ru/mkliman/Kliman_first_boot/KlimanFirstBootApplication.java
@@ -0,0 +1,13 @@
+package ru.mkliman.Kliman_first_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlimanFirstBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlimanFirstBootApplication.class, args);
+	}
+
+}
